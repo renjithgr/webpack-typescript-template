8b884459733631c3b86e4047b55a3a572018cd3b
Renjith <grenjithr@gmail.com>
2020-07-13 17:39:20 -0500

    TS build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts|\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [new HtmlWebpackPlugin()]
+  }
 };
